83049b7d13a4901b88ccd742cca34e7a4b3c07fa
djx <1181564186@qq.com>
2017-08-30 22:10:01 +0800

    e

--- manage.py
+++ manage.py
@@ -1,18 +1,43 @@
 import pickle
+import random
 from flask import Flask, render_template, request, session
 app = Flask(__name__)
 app.secret_key = 'lkdjkjf&*^%&(*(*))(775$%^^**(*_+&**&GBHJB'
 
+txt = [('select','Jinja2 uses a central object?','A item','B item','C item','D item','A'),
+        ('select','2 Jinja2 uses a central object?','A item','B item','C item','D item','A'),
+        ('select','3 Jinja2 uses a central object?','A item','B item','C item','D item','A'),
+        ('yorn','The simplest way to configure Jinja2 y','Y'),
+        ('yorn','2The simplest way to configure Jinja2 n','N')]
+
+def get_exam(exams):
+    if exams:
+        exam = exams[0]
+        exams = pickle.dumps(exams[1:])
+        return exam, exams
+
 @app.route("/hh")
 def hh():
-    content = [('A','jjdhhd'),('B','uuuehd')]
-    session['keyy'] = pickle.dumps(content)
-    return render_template('index.html')
+    flag = request.args.get('change', '')
+    if flag != 'next':
+        if 'curr_exam' in session:
+            exam = pickle.loads(session['curr_exam'])
+            return render_template('index.html',exam=exam)
+    if 'exams' not in session:
+        exams = txt[:]
+        random.shuffle(exams)
+    else:
+        exams = pickle.loads(session['exams'])
+    res = get_exam(exams)
+    if res:
+        exam, exams = res
+        session['exams'] = exams
+        session['curr_exam'] = pickle.dumps(exam)
+        return render_template('index.html',exam=exam)
+    return render_template('score.html',score='90')
 
 @app.route("/hc")
 def hc():
-    print(pickle.loads(session['keyy']))
-    
     return request.args.get('key', '')
 
 if __name__ == '__main__':
